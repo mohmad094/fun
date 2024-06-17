7599cc37d3d45d4c7d134e8c79513885324742d4
mohmad094 <116757782+mohmad094@users.noreply.github.com>
2024-06-17 15:08:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const yesButton = document.getElementById('yesButton');
+    const noButton = document.getElementById('noButton');
+    const message = document.getElementById('message');
+    let h1 = document.querySelector("h1");
+
+    yesButton.addEventListener('click', () => {
+        message.classList.remove('hidden');
+        h1.classList.add("hidden");
+        message.textContent = 'Congrats! Sabiha he is waiting for you';
+    });
+
+    noButton.addEventListener('click', () => {
+        const container = document.querySelector('.container');
+        const containerRect = container.getBoundingClientRect();
+        const buttonRect = noButton.getBoundingClientRect();
+        
+        const maxLeft = containerRect.width - buttonRect.width;
+        const maxTop = containerRect.height - buttonRect.height;
+
+        const newLeft = Math.random() * maxLeft;
+        const newTop = Math.random() * maxTop;
+
+        noButton.style.position = 'absolute';
+        noButton.style.left = `${newLeft}px`;
+        noButton.style.top = `${newTop}px`;
+    });
+});
